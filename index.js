a2dad19c51f2a865a3a219dbf3c31d9ca61e0766
Veeren2003 <102400884+Veeren2003@users.noreply.github.com>
2023-05-28 22:53:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,40 +3,23 @@ var numberOfDrumsButtons = document.querySelectorAll(".drum").length;
 for (var i = 0; i < numberOfDrumsButtons; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click",function () {
 
-    var buttonInnerHTML = this.innerHTML;
+for (var i = 0; i < numberOfDrumsButtons; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click",function () {
 
-    switch (buttonInnerHTML) {
-      case "w":
-        var tom3 = new Audio('sounds/tom-3.mp3');
-        tom3.play();
-        break;
-      case "a":
-       var kick-bass = new Audio('sounds/kick-bass.mp3');
-       kick-bass.play();
-       break;
-      case "s":
-       var snare = new Audio('sounds/snare.mp3');
-       snare.play();
-       break;
-      case "d":
-       var tom1 = new Audio('sounds/tom-1.mp3');
-       tom1.play();
-       break;
-      case "j":
-       var tom2 = new Audio('sounds/tom-2.mp3');
-       tom2.play();
-       break;
-      case "k":
-       var crash = new Audio('sounds/crash.mp3');
-       crash.play();
-       break;
-      case "l":
-       var tom4 = new Audio('sounds/tom-4.mp3');
-       tom4.play();
-       break;
-      default:
 
-    }
-  })
+        var buttonInnerHTML = this.textContent;
 
+        switch (buttonInnerHTML) {
+          case "w":
+            alert("I am done!");
+            break;
+          case "a":
+            alert("I am okay!");
+            break;
+          default:
+
+        }
+
+}
+);
 }
